ba2963826ae104fe46adbae24d2bc3f5c183c479
Angel de la Torre <angel.dlt.1996@gmail.com>
2020-06-06 14:14:35 -0700

    moved image folder to public folder for access

--- guessthemall/src/controller/imageController.js
+++ guessthemall/src/controller/imageController.js
@@ -2,18 +2,18 @@ import React from "react";
 import { Container, Row, Col } from "../components/Grid";
 import ImageCard from "../components/ImageCard";
 const imageArray = [
-  "../../images/dinosaur-1.png",
-  "../../images/dinosaur-2.png",
-  "../../images/dinosaur-3.png",
-  "../../images/dinosaur-4.png",
-  "../../images/dinosaur-5.png",
-  "../../images/dinosaur-6.png",
-  "../../images/dinosaur-7.png",
-  "../../images/dinosaur-8.png",
-  "../../images/dinosaur-9.png",
-  "../../images/dinosaur-10.png",
-  "../../images/dinosaur-11.png",
-  "../../images/dinosaur-12.png"
+  "/images/dinosaur-1.png",
+  "/images/dinosaur-2.png",
+  "/images/dinosaur-3.png",
+  "/images/dinosaur-4.png",
+  "/images/dinosaur-5.png",
+  "/images/dinosaur-6.png",
+  "/images/dinosaur-7.png",
+  "/images/dinosaur-8.png",
+  "/images/dinosaur-9.png",
+  "/images/dinosaur-10.png",
+  "/images/dinosaur-11.png",
+  "/images/dinosaur-12.png"
 ];
 
 function randomizeImages() {
