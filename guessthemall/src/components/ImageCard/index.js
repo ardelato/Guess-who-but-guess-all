976e2f7407bc6b6dd3c6878e326337f07986545e
Angel de la Torre <angel.dlt.1996@gmail.com>
2020-06-06 21:53:44 -0700

    Got the images to now score properly and randomize via event.target.value passthrough

--- guessthemall/src/components/ImageCard/index.js
+++ guessthemall/src/components/ImageCard/index.js
@@ -3,26 +3,29 @@ import Card from "react-bootstrap/Card";
 import "./style.css";
 
 class ImageCard extends Component {
-  state = {
-    checked: false
-  };
-
-  handleClick = cb => {
-    if (this.state.checked) {
-      console.log("Already Checked");
-      // console.log(cb);
-      cb(true);
-    } else {
-      this.setState({ checked: true });
-      // console.log(cb);
-      cb(false);
-    }
+  handleClick = (event, clickCB, scoreCB) => {
+    let domTarget =
+      event.target.getAttribute("class") === "card"
+        ? event.target
+        : event.target.parentElement;
+    clickCB(domTarget.getAttribute("value"), scoreCB);
   };
 
   render() {
     return (
-      <Card onClick={() => this.handleClick(this.props.scoreChange)}>
+      <Card
+        onClick={event =>
+          this.handleClick(
+            event,
+            this.props.handleChecked,
+            this.props.scoreChange
+          )
+        }
+        value={this.props.imageObj.src}
+      >
         <Card.Img variant="top" src={this.props.imageObj.src} />
+        <p>{this.props.imageObj.src}</p>
+        <p>{this.props.imageObj.checked.toString()}</p>
       </Card>
     );
   }
--- guessthemall/src/controller/imageController.js
+++ guessthemall/src/controller/imageController.js
@@ -21,9 +21,44 @@ class ImageLayout extends Component {
     ]
   };
 
-  componentDidMount() {
+  handleChecked = (srcID, scoreCB) => {
+    console.log("ID " + srcID);
+    let index = this.state.imageCards.findIndex(obj => obj.src === srcID);
+    if (this.state.imageCards[index].checked) {
+      console.log("Reset Array");
+      this.resetImages();
+      scoreCB(true);
+    } else {
+      console.log("Update Status");
+      let tempArray = this.state.imageCards;
+      tempArray[index].checked = true;
+      this.setState({ imageCards: tempArray });
+      this.randomizeImages();
+      scoreCB(false);
+    }
+    // if (imageStatus) {
+    //   console.log("Reset Array");
+    //   this.resetImages();
+    // } else {
+    //   console.log("Update Status");
+    //   console.log(this.state.imageCards.findIndex(obj => obj.src === srcID));
+    //   const index = this.state.imageCards.findIndex(obj => obj.src === srcID);
+
+    //   let tempArray = this.state.imageCards;
+    //   tempArray[index].checked = true;
+    //   this.setState({ imageCards: tempArray });
+    //   this.randomizeImages();
+    // }
+  };
+
+  resetImages = () => {
+    let images = this.state.imageCards;
+    images.forEach(image => {
+      image.checked = false;
+    });
+    this.setState({ imageCards: images });
     this.randomizeImages();
-  }
+  };
 
   randomizeImages = () => {
     let tempArray = this.state.imageCards.sort(() => Math.random() - 0.5);
@@ -40,24 +75,28 @@ class ImageLayout extends Component {
             <ImageCard
               imageObj={this.state.imageCards[0]}
               scoreChange={this.props.scoreChange}
+              handleChecked={this.handleChecked}
             ></ImageCard>
           </Col>
           <Col size="md-12">
             <ImageCard
               imageObj={this.state.imageCards[1]}
               scoreChange={this.props.scoreChange}
+              handleChecked={this.handleChecked}
             ></ImageCard>
           </Col>
           <Col size="md-12">
             <ImageCard
               imageObj={this.state.imageCards[2]}
               scoreChange={this.props.scoreChange}
+              handleChecked={this.handleChecked}
             ></ImageCard>
           </Col>
           <Col size="md-12">
             <ImageCard
               imageObj={this.state.imageCards[3]}
               scoreChange={this.props.scoreChange}
+              handleChecked={this.handleChecked}
             ></ImageCard>
           </Col>
         </Row>
@@ -66,24 +105,28 @@ class ImageLayout extends Component {
             <ImageCard
               imageObj={this.state.imageCards[4]}
               scoreChange={this.props.scoreChange}
+              handleChecked={this.handleChecked}
             ></ImageCard>
           </Col>
           <Col size="md-12">
             <ImageCard
               imageObj={this.state.imageCards[5]}
               scoreChange={this.props.scoreChange}
+              handleChecked={this.handleChecked}
             ></ImageCard>
           </Col>
           <Col size="md-12">
             <ImageCard
               imageObj={this.state.imageCards[6]}
               scoreChange={this.props.scoreChange}
+              handleChecked={this.handleChecked}
             ></ImageCard>
           </Col>
           <Col size="md-12">
             <ImageCard
               imageObj={this.state.imageCards[7]}
               scoreChange={this.props.scoreChange}
+              handleChecked={this.handleChecked}
             ></ImageCard>
           </Col>
         </Row>
@@ -92,24 +135,28 @@ class ImageLayout extends Component {
             <ImageCard
               imageObj={this.state.imageCards[8]}
               scoreChange={this.props.scoreChange}
+              handleChecked={this.handleChecked}
             ></ImageCard>
           </Col>
           <Col size="md-12">
             <ImageCard
               imageObj={this.state.imageCards[9]}
               scoreChange={this.props.scoreChange}
+              handleChecked={this.handleChecked}
             ></ImageCard>
           </Col>
           <Col size="md-12">
             <ImageCard
               imageObj={this.state.imageCards[10]}
               scoreChange={this.props.scoreChange}
+              handleChecked={this.handleChecked}
             ></ImageCard>
           </Col>
           <Col size="md-12">
             <ImageCard
               imageObj={this.state.imageCards[11]}
               scoreChange={this.props.scoreChange}
+              handleChecked={this.handleChecked}
             ></ImageCard>
           </Col>
         </Row>
