877b51f8699acaddc7d1cc2fc56a120d10299831
Angel de la Torre <angel.dlt.1996@gmail.com>
2020-06-06 22:04:10 -0700

    Updates

---
+++ precache-manifest.88dbce52a180796269b5aebb34fcec3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb67d19fb221ed1906e44084529bc70a",
+    "url": "/Guess-who-but-guess-all/index.html"
+  },
+  {
+    "revision": "357ded36355aca061fb0",
+    "url": "/Guess-who-but-guess-all/static/css/main.3cc5a97a.chunk.css"
+  },
+  {
+    "revision": "d2929a4649d2bf6bb34c",
+    "url": "/Guess-who-but-guess-all/static/js/2.dc2082e7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Guess-who-but-guess-all/static/js/2.dc2082e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "357ded36355aca061fb0",
+    "url": "/Guess-who-but-guess-all/static/js/main.b7d04161.chunk.js"
+  },
+  {
+    "revision": "c6a127ad4aa885b31641",
+    "url": "/Guess-who-but-guess-all/static/js/runtime-main.eccf4ede.js"
+  }
+]);
